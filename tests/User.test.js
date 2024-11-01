90b8b2cd85288ce6d23ad8298df7799a00f5953a
James Nguyen <james.nguyen@james.nguyen-MacBook-Pro>
2024-11-01 14:47:30 -0700

    User unit test to ensure proper functionality for classes

--- tests/User.test.js
+++ tests/User.test.js
@@ -2,18 +2,39 @@ const User = require('../src/User');
 
 const user = new User("Joe Bloggs", "test123", 21);
 
-// User tests here
-describe("User property tests", () => {
-  // test username
-  test("username should be a string", () => {
-    expect(typeof user.username).toBe("string");
-  })
-  // test password
+const User = require('../src/User');
 
-  // test age
-})
+const user = new User("Joe Bloggs", "test123", 21);
 
 
-// test login
+describe("User", () => {
+  // Instantiating a user varialbe for testing
+  let testUser;
+  beforeEach(() => {
+    testUser = new User("jnguyen", "jalynguy", 26);
+  });
 
-// test logout
+  // User tests here
+  describe("User property tests", () => {
+    // test username
+    it("should have a username property with a string value", () => {
+      expect(testUser).toHaveProperty("username", "jnguyen");
+  });
+
+  it("should have a password property with a string value", () => {
+      expect(testUser).toHaveProperty("password", "jalynguy");
+  });
+
+  it("should have an age property with a number value", () => {
+      expect(testUser).toHaveProperty("age", 26);
+  });
+
+  it("should have a loggedIn property with a boolean value", () => {
+      expect(testUser).toHaveProperty("loggedIn", false);
+  });
+  
+  it("should start off not logged in", () => {
+      expect(testUser.loggedIn).toBe(false);
+  });
+
+});
