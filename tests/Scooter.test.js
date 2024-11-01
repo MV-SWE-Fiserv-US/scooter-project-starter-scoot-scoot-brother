eb5d713157fe28c3c0aacd9768555105ba55e778
Paola Milanes <paola.milanes@paola.milanes-MacBook-Pro>
2024-11-01 14:37:28 -0700

    added test to scooter.test

--- tests/Scooter.test.js
+++ tests/Scooter.test.js
@@ -1,25 +1,89 @@
-const Scooter = require('../src/Scooter')
-const User = require('../src/User')
+const { describe, it, expect } = require("@jest/globals");
+const Scooter = require("../src/Scooter.js");
+const User = require("../src/User.js");
 
-//typeof scooter === object
-describe('scooter object', () => {
-  test('Scooter class should create Scooter instance', () => {
-    const scooter = new Scooter();
-    expect(scooter).toBeInstanceOf(Scooter);
+describe("Scooter", () => {
+  let testScooterInUse;
+  let testScooterBroken;
+  let testUser1;
+  let testUser2;
+  let testUser3;
+
+  beforeEach(() => {
+    testScooterInUse = new Scooter("Union Station");
+    testScooterLowCharge = new Scooter("Convention Center");
+    testScooterBroken = new Scooter("Convention Center");
+
+    testUser1 = new User("testUser1", "password", 22);
+    testUser2 = new User("testUser2", "password", 25);
+    testUser3 = new User("testUser3", "password", 27);
+
+    testScooterInUse.rent(testUser1);
+
+    testScooterLowCharge.charge = 11;
+
+    testScooterBroken.isBroken = true;
   });
-})
 
-//Method tests
-describe('scooter methods', () => {
-  // tests here!
+    it("should have a station property with a null value if checked out", () => {
+      expect(testScooterInUse).toHaveProperty("station", null);
+    });
 
-  //rent method
-  
+    it("should have a user property with a User instance if checked out", () => {
+      expect(testScooterInUse).toHaveProperty("user", testUser1);
+    });
 
-  //dock method
+    it("should have a nextSerial static property with a number value starting at 1 and incrementing for each instance of Scooter created", () => {
+      expect(Scooter.nextSerial).toBe(31);
+    });
 
-  //requestRepair method
+  });
 
-  //charge method
+  describe("Scooter methods", () => {
+    describe("rent(user) method", () => {
+      it("should rent to user if Scooter is above 20% charge and not broken", () => {
+        expect(testScooterInUse.user).toEqual(testUser1);
+        expect(testScooterInUse.user).toBeInstanceOf(User);
+      });
 
-})
+      it("should handle errors for insufficient charge", () => {
+        expect(() => {
+          testScooterLowCharge.rent(testUser2);
+        }).toThrow("scooter needs to charge");
+      });
+
+      it("should handle errors for broken Scooters", () => {
+        expect(() => {
+          testScooterBroken.rent(testUser3);
+        }).toThrow("scooter needs repair");
+      });
+    });
+
+    describe("dock(station) method", () => {
+      it("should return scooter to the station", () => {
+        testScooterInUse.dock("Union Station");
+        expect(testScooterInUse.station).toBe("Union Station");
+      });
+
+      it("should clear the user", () => {
+        testScooterInUse.dock("Union Station");
+        expect(testScooterInUse.user).toBeNull();
+      });
+    });
+
+    describe("recharge() method", () => {
+      it("should recharge a Scooter instance", async () => {
+        await testScooterLowCharge.recharge();
+        expect(testScooterLowCharge.charge).toBe(100);
+      });
+    });
+
+    describe("requestRepair() method", () => {
+      it("should repair a Scooter instance", async () => {
+        const scooter = new Scooter();
+        scooter.isBroken = true;
+        await scooter.requestRepair();
+        expect(scooter.isBroken).toBe(false);
+      });
+    });
+  });
