e293ce23c8ab8239f45192f024f7f327498b0cf1
Paola Milanes <paola.milanes@paola.milanes-MacBook-Pro>
2024-11-01 14:26:03 -0700

    finished scooter.js

--- src/Scooter.js
+++ src/Scooter.js
@@ -8,6 +8,40 @@ class Scooter{
       this.isBroken = false;
       Scooter.nextSerial += 1;
   }
+
+  rent(user) {
+    if(this.charge <= 20 && this.isBroken === false) {
+        throw new Error("scooter needs to charge");
+    }
+    if(this.charge > 20 && this.isBroken === true) {
+        throw new Error("scooter needs repair");
+    }
+    this.station = null;
+    this.user = user;
+}
+
+dock(station) {
+    this.station = station;
+    this.user = null; 
+}
+
+async recharge() {
+    console.log('Starting charge');
+
+    await new Promise(resolve => setTimeout(resolve, 3000)); 
+    this.charge = 100;
+
+    console.log('Charge complete');
+}
+
+async requestRepair() {
+    console.log('Starting charge');
+
+    await new Promise(resolve => setTimeout(resolve, 3000)); 
+    this.isBroken = false;
+
+    console.log('Repair complete');
+  }
 }
 
 
