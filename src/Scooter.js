b86b0ff5939c646aa446873520ede70cbf6664b4
Paola Milanes <paola.milanes@paola.milanes-MacBook-Pro>
2024-11-01 14:16:32 -0700

    created a scooter class

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,5 +1,13 @@
 class Scooter{
-  // scooter code here
+  static nextSerial = 1;
+  constructor(station) {
+      this.station = station;
+      this.user = null;
+      this.serial = Scooter.nextSerial;
+      this.charge = 100;
+      this.isBroken = false;
+      Scooter.nextSerial += 1;
+  }
 }
 
 
